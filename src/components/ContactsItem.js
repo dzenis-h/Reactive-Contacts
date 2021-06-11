370919f5c5e70e588dac1ec9dccc304cd19eb746
BiggaHD <dzenis.hankusic@gmail.com>
2021-06-11 19:46:56 +0200

    Small styling change

--- src/components/ContactsItem.js
+++ src/components/ContactsItem.js
@@ -32,7 +32,8 @@ const ContactsItem = ({ contact: { name, phone, email, type, id } }) => {
       <div className={classes.card}>
         <ul>
           <li>
-            <i className="fas fa-file-signature" /> Name: <span>{name}</span>
+            <i className="fas fa-file-signature" /> {'  '}Name:{' '}
+            <span>{name}</span> <br />
             {isEditing && (
               <input value={value.name} onChange={onChange} name="name" />
             )}
@@ -40,7 +41,7 @@ const ContactsItem = ({ contact: { name, phone, email, type, id } }) => {
           <li className={classes.icons}>
             <i className="fas fa-mobile-alt" /> {'  '}
             Phone: {'  '}
-            <span>{phone}</span>
+            <span>{phone}</span> <br />
             {isEditing && (
               <input value={value.phone} onChange={onChange} name="phone" />
             )}
@@ -49,6 +50,7 @@ const ContactsItem = ({ contact: { name, phone, email, type, id } }) => {
             <i className="far fa-envelope" />
             {'  '} E-mail:
             <span>{email}</span>
+            <br />
             {isEditing && (
               <input value={value.email} onChange={onChange} name="email" />
             )}
