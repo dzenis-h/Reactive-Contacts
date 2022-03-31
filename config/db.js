ebbed17b1db701e9334b10f6a00bdd2d762da8d6
Dzenis <dzenis.hankusic@gmail.com>
2022-03-31 08:01:50 +0200

    V2.0

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const mongoURI = process.env.mongoURI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
