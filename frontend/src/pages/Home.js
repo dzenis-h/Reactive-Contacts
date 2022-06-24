e1b1866c67db0ee48e5cdd0d3b53f483b1e8c8eb
Dzenis <dzenis.hankusic@gmail.com>
2022-06-24 11:04:41 +0200

    XO

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,9 @@
-import React, { useContext, Fragment } from 'react';
-import ContactsForm from '../components/ContactsForm';
-import classes from './Home.module.css';
-import ContactsList from '../components/ContactsList';
-import { AuthContext } from '../store/auth-context';
-import { Redirect } from 'react-router-dom';
+import React, { useContext, Fragment } from "react";
+import ContactsForm from "../components/ContactsForm";
+import classes from "./Home.module.css";
+import ContactsList from "../components/ContactsList";
+import { AuthContext } from "../store/auth-context";
+import { Redirect } from "react-router-dom";
 
 const Home = () => {
   const { isAuth } = useContext(AuthContext);
